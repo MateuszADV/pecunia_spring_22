92d6763e8ec6bb45de6e20abddb6f2dc9e42c701
MateuszADV <mateusz.adv@gmail.com>
2024-06-15 17:34:17 +0200

    Dodano Całe Note future

---
+++ src/main/java/pecunia_22/controllers/viewControllers/NoteFutureController.java
@@ -0,0 +1,58 @@
+package pecunia_22.controllers.viewControllers;
+
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pecunia_22.models.repositories.NoteRepository;
+import pecunia_22.models.sqlClass.CountryByStatus;
+import pecunia_22.models.sqlClass.GetNotesByStatus;
+import utils.JsonUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class NoteFutureController {
+
+    private NoteRepository noteRepository;
+
+    @GetMapping("/note/future")
+    public String getindex(ModelMap modelMap) {
+        List<Object[]> objects;
+        List<CountryByStatus> countryByStatusList = new ArrayList<>();
+        objects = noteRepository.countryByStatus("FUTURE");
+        try {
+            System.out.println(JsonUtils.gsonPretty(objects));
+            for (Object[] object : objects) {
+                countryByStatusList.add(new ModelMapper().map(object[0], CountryByStatus.class));
+            }
+
+            modelMap.addAttribute("countryByStatusList", countryByStatusList);
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return "note/futureNote/index";
+    }
+
+    @GetMapping("/note/future/country/notes/{countryId}")
+    public String getCountryNotes(@PathVariable("countryId") Long countryId, ModelMap modelMap) {
+        System.out.println("[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]C");
+        System.out.println(countryId);
+        System.out.println("[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]C");
+
+        List<Object[]> objects;
+        objects = noteRepository.getNotesByStatus("FUTURE", countryId);
+        List<GetNotesByStatus> getNotesByStatusList = new ArrayList<>();
+        for (Object[] object : objects) {
+            getNotesByStatusList.add(new ModelMapper().map(object[0],GetNotesByStatus.class));
+        }
+
+        modelMap.addAttribute("futureNotesList", getNotesByStatusList);
+        System.out.println(JsonUtils.gsonPretty(getNotesByStatusList));
+        return "note/futureNote/notes";
+    }
+}
