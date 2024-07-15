d8d377822677fbb2db2aa9d3e01b17d026869df7
mateuszadv <mateusz.adv@gmail.com>
2024-07-15 16:57:15 +0200

    dodano rwszte setting

--- src/main/java/pecunia_22/controllers/viewControllers/SettingController.java
+++ src/main/java/pecunia_22/controllers/viewControllers/SettingController.java
@@ -8,11 +8,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import pecunia_22.models.Pattern;
+import pecunia_22.models.ShippingType;
 import pecunia_22.models.dto.pattern.PatternDto;
 import pecunia_22.models.dto.seting.SettingDto;
 import pecunia_22.models.dto.seting.SettingDtoForm;
+import pecunia_22.models.dto.shippingType.ShippingTypeDtoForm;
 import pecunia_22.models.setting.Setting;
 import pecunia_22.services.setingService.SettingServiceImpl;
 import utils.JsonUtils;
@@ -57,5 +60,33 @@ public class SettingController {
 
         return "redirect:/setting";
 //        return getShow(pattern.getId(), modelMap);
+    }
+    @GetMapping("/setting/edit/{settingId}")
+    public String getEdit(@PathVariable Long settingId, ModelMap modelMap) {
+        Setting setting = settingService.getSettingById(settingId);
+        SettingDtoForm settingDtoForm = new ModelMapper().map(setting, SettingDtoForm.class);
+        modelMap.addAttribute("settingForm", settingDtoForm);
+        return "setting/setting/edit";
+    }
+
+    @PostMapping("/setting/edit")
+    public String postEdit(@ModelAttribute("settingForm")@Valid SettingDtoForm settingForm, BindingResult result, ModelMap modelMap ){
+        if(result.hasErrors()) {
+            return "setting/setting/edit";
+        }
+
+        Setting setting = new ModelMapper().map(settingForm, Setting.class);
+        System.out.println("--------------------------------------");
+        System.out.println(JsonUtils.gsonPretty(setting));
+        System.out.println("---------------------------------------");
+        settingService.saveSetting(setting);
+//        return getShow(pattern.getId(), modelMap);
+        return "redirect:/setting";
+    }
+
+    @GetMapping("/setting/delete/{settingId}")
+    public String delete(@PathVariable Long settingId, ModelMap modelMap) {
+        settingService.deleteSettingById(settingId);
+        return "redirect:/setting";
     }
 }
