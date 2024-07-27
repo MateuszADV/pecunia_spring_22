ddc4188c5ad07586ea73a554b26ad2c4337d2a65
mateuszadv <mateusz.adv@gmail.com>
2024-07-27 15:15:52 +0200

    poprawki update note CD..

--- src/main/java/pecunia_22/services/noteServices/NoteServiceImpl.java
+++ src/main/java/pecunia_22/services/noteServices/NoteServiceImpl.java
@@ -166,6 +166,8 @@ public class NoteServiceImpl implements NoteService {
     public void updateNote(Note note) {
         noteRepository.updateNote(note.getCurrencies().getId(), note.getDenomination(), note.getDateBuy(), note.getNameCurrency(), note.getSeries(),
                 note.getBoughts().getId(), note.getItemDate(), note.getQuantity(), note.getUnitQuantity(), note.getActives().getId(), note.getPriceBuy(), note.getPriceSell(),
+                note.getMakings().getId(), note.getQualities().getId(), note.getWidth(), note.getHeight(), note.getStatuses().getId(), note.getImageTypes().getId(),
+                note.getStatusSell(), note.getVisible(), note.getDescription(), note.getAversPath(), note.getReversePath(),
                 note.getId());
     }
 }
