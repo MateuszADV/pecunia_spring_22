f46896bfc33f7fd0e8723e8fceaeefcf505ae95c
MateuszADV <mateusz.adv@gmail.com>
2026-02-08 13:55:19 +0100

    Refaktoring sql w coin poprawki 2026-02-08

--- src/main/java/pecunia_22/models/repositories/CoinRepository.java
+++ src/main/java/pecunia_22/models/repositories/CoinRepository.java
@@ -131,29 +131,6 @@ ORDER BY cur.currencySeries
             "ORDER BY coin.denomination")
     Page<Coin> coinPageable(Long currencyId, String status, Boolean visible, final Pageable pageable);
 
-//    @Query(value = "SELECT new map(coin.qualities AS qualities, coin.id AS coinId, cou.id AS countryId, cou.countryEn AS countryEn, cou.countryPl AS countryPl, cur.id AS currencyId, " +
-//            "cur.currencySeries AS currencySeries, cur.patterns AS patterns, bou.name AS bought, coin.denomination AS denomination, coin.nameCurrency AS nameCurrency, coin.itemDate AS itemDate, " +
-//            "coin.priceBuy AS priceBuy, coin.priceSell AS priceSell, coin.quantity AS quantity, coin.unitQuantity AS unitQuantity, " +
-//            "coin.diameter AS diameter, coin.thickness AS thickness, coin.weight AS weight, " +
-//            "coin.visible AS visible, coin.composition AS composition, coin.description AS description, " +
-//            "coin.aversPath AS aversPath, coin.reversePath AS reversePath ) " +
-//            "  FROM Coin coin" +
-//            "  LEFT JOIN Status stat" +
-//            "    ON stat.status = ?1" +
-//            "  LEFT JOIN Bought bou" +
-//            "    ON bou = coin.boughts" +
-//            "  LEFT JOIN Currency cur" +
-//            "    ON cur = coin.currencies" +
-//            "  LEFT JOIN Country cou" +
-//            "    ON cou = cur.countries" +
-//            "  LEFT JOIN Quality qua" +
-//            "    ON qua = coin.qualities" +
-//            " WHERE stat = coin.statuses" +
-//            " GROUP BY coin.qualities, coin.id, cou.id, cou.countryEn, cou.countryPl, cur.id, cur.currencySeries, cur.patterns, bou.name, coin.denomination, coin.nameCurrency, coin.itemDate, " +
-//            "          coin.priceBuy, coin.priceSell, coin.quantity, coin.unitQuantity, coin.visible, coin.description, coin.aversPath, coin.reversePath " +
-//            " ORDER BY cou.countryEn, coin.denomination")
-//    List<Object[]> getCoinsByStatus(String status);
-
     @Query("""
                 SELECT new map(
                     coin.qualities      AS qualities,
